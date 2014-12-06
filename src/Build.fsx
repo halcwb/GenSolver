0d0f826a08be6cf15e198a493ed7dfe8c07e1bba
halcwb <halcwb@gmail.com>
2014-12-06 20:25:54 +0100

    New folder struc + adding fake

---
+++ src/Build.fsx
@@ -0,0 +1,9 @@
+﻿#r @"packages\FAKE.3.11.3\tools\FakeLib.dll"
+
+open Fake
+
+Target "Default" (fun _ ->
+    trace "Building GenSolver"
+)
+
+RunTargetOrDefault "Default"
---
+++ src/build.bat
@@ -0,0 +1,4 @@
+@echo off
+cls
+"packages\FAKE.3.11.3\tools\Fake.exe" build.fsx
+pause
