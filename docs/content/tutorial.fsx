5ab73e4d07f0d15d250c5b3b1123f22ccdc244cc
Casper Bollen <halcwb@gmail.com>
2015-11-20 12:27:05 +0100

    Added first tutorial

---
+++ docs/content/tutorial.fsx
@@ -0,0 +1,15 @@
+﻿(*** Hide ***)
+#I "../../src/Informedica.GenSolver.Lib/Scripts"
+#load "load-project.fsx"
+
+(**
+# How to use this library
+*)
+
+open Informedica.GenSolver.Lib
+
+(**
+## How to create a value
+*)
+
+let value = Variable.Value.create 1N
