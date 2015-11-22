3d0be64faf79477153f51d12698b19b2e5ab27a7
Casper Bollen <halcwb@gmail.com>
2015-11-22 13:58:00 +0100

    Moved testing code up one level to enable use in scripting

--- tests/Informedica.GenSolver.Tests/Variable.fs
+++ tests/Informedica.GenSolver.Tests/Variable.fs
@@ -6,68 +6,70 @@ open NUnit.Framework
 
 module Testing =
 
-    module Value =
+    module Variable =
 
-        open Variable.Value
-        open Variable.ValueSet
+        module Value =
+
+            open Variable.Value
+            open Variable.ValueSet
         
-        [<TestFixture>]
-        type ``Given a zero or negative number`` () =
-            [<Test>]
-            member x.``With 0 an exception is raised`` () =
-                raises<Variable.Value.NonZeroOrPositiveValueException> <@ 0N |> Variable.Value.create <> (0N |> Value.Value) @>
-            [<Test>]
-            member x.``With -1N an exception is raised`` () =
-                raises<Variable.Value.NonZeroOrPositiveValueException> <@ 0N |> Variable.Value.create <> (-1N |> Value.Value) @>
-        [<TestFixture>]
-        type ``Given a non zero positive value`` () =
-            [<Test>]
-            member x.``A value can be created`` () =
-                test<@ Variable.Value.create 1N = (1N |> Value.Value) @>
+            [<TestFixture>]
+            type ``Given a zero or negative number`` () =
+                [<Test>]
+                member x.``With 0 an exception is raised`` () =
+                    raises<Variable.Value.NonZeroOrPositiveValueException> <@ 0N |> Variable.Value.create <> (0N |> Value.Value) @>
+                [<Test>]
+                member x.``With -1N an exception is raised`` () =
+                    raises<Variable.Value.NonZeroOrPositiveValueException> <@ 0N |> Variable.Value.create <> (-1N |> Value.Value) @>
+            [<TestFixture>]
+            type ``Given a non zero positive value`` () =
+                [<Test>]
+                member x.``A value can be created`` () =
+                    test<@ Variable.Value.create 1N = (1N |> Value.Value) @>
         
-        [<TestFixture>]
-        type ``Given an infix operand`` () =
+            [<TestFixture>]
+            type ``Given an infix operand`` () =
             
-            [<Test>]
-            member x.``The operand gives the same result as applied to BigRationals`` () =
-                let v1 = 1N |> Variable.Value.create
-                let v2 = 1N |> Variable.Value.create
-                test <@ Variable.Value.calc (+) v1 v2 |> Variable.Value.getValue = 2N @>
-                test <@ (v1 + v2) |> Variable.Value.getValue = 2N @>
+                [<Test>]
+                member x.``The operand gives the same result as applied to BigRationals`` () =
+                    let v1 = 1N |> Variable.Value.create
+                    let v2 = 1N |> Variable.Value.create
+                    test <@ Variable.Value.calc (+) v1 v2 |> Variable.Value.getValue = 2N @>
+                    test <@ (v1 + v2) |> Variable.Value.getValue = 2N @>
 
 
-    module Values =
+        module Values =
 
-        open  Variable.ValueSet
+            open  Variable.ValueSet
 
-        [<TestFixture>]
-        type ``Given list = empty incr = None min = None max = None`` () =
-            let incr = None
-            let min = None
-            let max = None
+            [<TestFixture>]
+            type ``Given list = empty incr = None min = None max = None`` () =
+                let incr = None
+                let min = None
+                let max = None
 
-            let vals = Range.All |> ValueSet.Range
+                let vals = Range.All |> ValueSet.Range
         
-            [<Test>]
-            member x.``Creating values returns range All`` () =
-                test <@ Variable.ValueSet.create incr min max [] = vals @>
+                [<Test>]
+                member x.``Creating values returns range All`` () =
+                    test <@ Variable.ValueSet.create incr min max [] = vals @>
         
-            [<Test>]
-            member x.``Counting values returns one`` () =
-                test <@ Variable.ValueSet.create incr min max [] |> Variable.ValueSet.count = 0 @>
+                [<Test>]
+                member x.``Counting values returns one`` () =
+                    test <@ Variable.ValueSet.create incr min max [] |> Variable.ValueSet.count = 0 @>
 
-        [<TestFixture>]
-        type ``Given list with one value incr = None min = None max = None`` () =
-            let incr = None
-            let min = None
-            let max = None
+            [<TestFixture>]
+            type ``Given list with one value incr = None min = None max = None`` () =
+                let incr = None
+                let min = None
+                let max = None
 
-            let vals = [1N |> Variable.Value.create] 
+                let vals = [1N |> Variable.Value.create] 
 
-            [<Test>]
-            member x.``Counting values returns one`` () =
-                test <@ Variable.ValueSet.create incr min max vals |> Variable.ValueSet.count = 1 @>
+                [<Test>]
+                member x.``Counting values returns one`` () =
+                    test <@ Variable.ValueSet.create incr min max vals |> Variable.ValueSet.count = 1 @>
         
-            [<Test>]
-            member x.``Creating values returns one value`` () =
-                test <@ Variable.ValueSet.create incr min max vals = (vals |> Variable.ValueSet.seqToValueSet) @>
+                [<Test>]
+                member x.``Creating values returns one value`` () =
+                    test <@ Variable.ValueSet.create incr min max vals = (vals |> Variable.ValueSet.seqToValueSet) @>
