860ce5373353ff62f36e6aa107db1432354b9ea9
Casper Bollen <halcwb@gmail.com>
2015-11-22 13:56:12 +0100

    Move scripting and testing to test project

--- tests/Informedica.GenSolver.Tests/Script.fsx
+++ tests/Informedica.GenSolver.Tests/Script.fsx
@@ -1,8 +1,95 @@
 ﻿// Learn more about F# at http://fsharp.net. See the 'F# Tutorial' project
 // for more guidance on F# programming.
-
-#load "Library1.fs"
-open Informedica.GenSolver.Tests
+#I "../../src/Informedica.GenSolver.Lib/"
+#I "../../src/Informedica.GenSolver.Lib/Scripts/"
+#load "load-references.fsx"
+#load "Variable.fs"
 
 // Define your library scripting code here
+open Informedica.GenSolver.Lib
+open Swensen.Unquote
 
+
+let v1 = 2N |> Variable.Value.create
+let v2 = 1N |> Variable.Value.create
+v1 + v2
+v1 - v2
+v1 * v2
+v1 / v2
+v1 > v2
+
+module Testing =
+
+    open Variable.Value
+    open Variable.ValueSet
+
+    type ``Given a zero or negative number`` () =
+
+        member x.``With 0 an exception is raised`` () =
+            raises<Variable.Value.NonZeroOrPositiveValueException> <@ 0N |> Variable.Value.create <> (0N |> Value.Value) @>
+
+        member x.``With -1N an exception is raised`` () =
+            raises<Variable.Value.NonZeroOrPositiveValueException> <@ 0N |> Variable.Value.create <> (-1N |> Value.Value) @>
+
+    type ``Given a non zero positive value`` () =
+        
+        member x.``A value can be created`` () =
+            test<@ Variable.Value.create 1N = (1N |> Value.Value) @>
+
+    type ``Given an infix operand`` () =
+        
+        member x.``The operand gives the same result as applied to BigRationals`` () =
+            let v1 = 1N |> Variable.Value.create
+            let v2 = 1N |> Variable.Value.create
+            test <@ Variable.Value.calc (+) v1 v2 |> Variable.Value.getValue = 2N @>
+            test <@ (v1 + v2) |> Variable.Value.getValue = 2N @>
+
+
+    type ``Given list = empty incr = None min = None max = None`` () =
+        let incr = None
+        let min = None
+        let max = None
+
+        let vals = Range.All |> ValueSet.Range
+        
+        member x.``Creating values returns range All`` () =
+            test <@ Variable.ValueSet.create incr min max [] = vals @>
+        
+        member x.``Counting values returns one`` () =
+            test <@ Variable.ValueSet.create incr min max [] |> Variable.ValueSet.count = 0 @>
+
+    type ``Given list with one value incr = None min = None max = None`` () =
+        let incr = None
+        let min = None
+        let max = None
+
+        let vals = [1N |> Variable.Value.create] 
+
+        
+        member x.``Counting values returns one`` () =
+            test <@ Variable.ValueSet.create incr min max vals |> Variable.ValueSet.count = 1 @>
+        
+        member x.``Creating values returns one value`` () =
+            test <@ Variable.ValueSet.create incr min max vals = (vals |> Variable.ValueSet.seqToValueSet) @>
+
+
+let runTests () =
+
+    let test = new Testing.``Given a zero or negative number``()
+    test.``With 0 an exception is raised``()
+    test.``With -1N an exception is raised``()
+    
+    let test = new Testing.``Given a non zero positive value``()
+    test.``A value can be created``()
+
+    let test = new Testing.``Given an infix operand``()
+    test.``The operand gives the same result as applied to BigRationals``()
+
+    let test = new Testing.``Given list = empty incr = None min = None max = None``()
+    test.``Counting values returns one``()
+    test.``Creating values returns range All``()
+
+    let test = new Testing.``Given list with one value incr = None min = None max = None``()
+    test.``Counting values returns one``()
+    test.``Creating values returns one value``()
+    
