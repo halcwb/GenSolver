25bf22ad05b767f2ed76413123c519cad5f2a451
Casper Bollen <halcwb@gmail.com>
2016-05-07 22:50:49 +0200

    Updated assemblyinfo

--- src/Informedica.GenSolver.Lib/AssemblyInfo.fs
+++ src/Informedica.GenSolver.Lib/AssemblyInfo.fs
@@ -3,6 +3,7 @@ open System.Reflection
 
 [<assembly: AssemblyTitleAttribute("Informedica.GenSolver.Lib")>]
 [<assembly: AssemblyProductAttribute("GenSolver")>]
+[<assembly: AssemblyCompanyAttribute("halcwb")>]
 [<assembly: AssemblyDescriptionAttribute("A solver that solves a set of product and sum equations in which variables are discrete sets of rational numbers that cannot be zero or negative")>]
 [<assembly: AssemblyVersionAttribute("0.1.0")>]
 [<assembly: AssemblyFileVersionAttribute("0.1.0")>]
