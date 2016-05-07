9e44953cde112236bfcb654d2f0910fc73b99f36
Casper Bollen <halcwb@gmail.com>
2016-05-07 15:04:18 +0200

    Bump version to 0.1.0-beta

--- src/Informedica.GenSolver.Lib/AssemblyInfo.fs
+++ src/Informedica.GenSolver.Lib/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Informedica.GenSolver.Lib")>]
 [<assembly: AssemblyProductAttribute("GenSolver")>]
 [<assembly: AssemblyDescriptionAttribute("A solver that solves a set of product and sum equations in which variables are discrete sets of rational numbers that cannot be zero or negative")>]
-[<assembly: AssemblyVersionAttribute("0.0.2")>]
-[<assembly: AssemblyFileVersionAttribute("0.0.2")>]
+[<assembly: AssemblyVersionAttribute("0.1.0")>]
+[<assembly: AssemblyFileVersionAttribute("0.1.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.0.2"
+    let [<Literal>] Version = "0.1.0"
